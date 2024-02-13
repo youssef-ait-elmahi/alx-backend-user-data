3e05c301589adcef9c3d7c87c010aad56c7bb825
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-02-13 16:38:10 +0100

    Refactor require_auth method in Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,14 +1,18 @@
-#!/usr/bin/env python3
-""" Authentication module"""
-from flask import request
-from typing import List, TypeVar
-
-
 class Auth:
     """Authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to check if auth is required"""
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path or excluded_path.startswith(path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get authorization header"""
