4f909f1858e8a69d089092abfc7dfd4fa6187518
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-02-13 17:01:47 +0100

    Update authentication module

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python3
+""" Authentication module. """
+from flask import request
+from typing import List, TypeVar
+
+
 class Auth:
-    """Authentication class"""
+    """Authentication class."""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if auth is required"""
+        """ Method to check if auth is required. """
         if path is None:
             return True
 
@@ -15,11 +21,11 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Method to get authorization header"""
+        """ Method to get authorization header. """
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Method to get user from request"""
+        """ Method to get user from request. """
         return None
